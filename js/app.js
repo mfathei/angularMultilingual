6d2fa285b01a55841355f4acb41aefb1be8e75f0
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-06-19 01:27:30 +0200

    enable switch language button

--- js/app.js
+++ js/app.js
@@ -21,3 +21,32 @@ app.config(['$translateProvider', function($translateProvider){
     $translateProvider.useMissingTranslationHandlerLog();
     
 }]);
+
+app.run(['$rootScope', function($rootScope){
+    $rootScope.lang = 'en';
+    
+    $rootScope.default_float = 'left';
+    $rootScope.opposite_float = 'right';
+    
+    $rootScope.default_direction = 'ltr';
+    $rootScope.opposite_direction = 'rtl';
+}]);
+
+app.controller('LanguageSwitchController', ['$scope', '$rootScope', '$translate', function($scope, $rootScope, $translate){
+    $scope.changeLanguage = function(langKey){
+        $translate.use(langKey);
+    };
+    
+    $rootScope.$on('$translateChangeSuccess', function(event, data){
+        var language = data.language;
+        
+        $rootScope.lang = language;
+        
+        $rootScope.default_direction = language === 'ar'? 'rtl':'ltr';
+        $rootScope.opposite_direction = language === 'ar'? 'ltr':'rtl';
+        
+        $rootScope.default_float = language === 'ar'? 'right':'left';
+        $rootScope.opposite_float = language === 'ar'? 'left':'right';
+    });
+    
+}]);
